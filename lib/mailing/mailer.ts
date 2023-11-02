c000927fdd5de0686c44084c813c85f0fc11f56d
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2023-11-02 20:57:23 +0530

    Added mailing backend

---
+++ lib/mailing/mailer.ts
@@ -0,0 +1,31 @@
+"use server";
+import nodemailer from "nodemailer";
+
+const sender = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+export async function sendmail({
+  to,
+  subject,
+  text = "HI",
+  html,
+}: {
+  to: string;
+  subject?: string;
+  text?: string;
+  html?: string;
+}) {
+  const info = await sender.sendMail({
+    from: process.env.MAIL_USER,
+    to,
+    subject,
+    text,
+    html,
+  });
+  console.log("Message sent: %s", info.messageId);
+}
